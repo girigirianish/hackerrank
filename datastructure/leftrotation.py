428b362ee6e9f095c816e47ad3e13cefd5c2929e
anish <giri.girianish@gmail.com>
2017-09-02 11:19:50 +0545

    added comments and modified output in a string as expected

--- datastructure/leftrotation.py
+++ datastructure/leftrotation.py
@@ -1,3 +1,13 @@
+#@Function Name : leftrotation
+#@Description   : Rotates the given list with respect to the rotation ratio given 
+#
+#@Param 		: this function takes a string as and input . 
+#				  The string is compose of the list that is to be rotated 
+#				  and the ratio of in which the list is to be rotated. 
+#
+#Example		: Input : "1 2 3 4 5 /n 5" Output : 5 1 2 3 4
+
+
 from collections import deque
 
 def leftrotation(lrinput):
@@ -8,7 +18,7 @@ def leftrotation(lrinput):
 
 	listToRotate.rotate(-4)
 
-	print list(listToRotate)
-	return
+	print ' '.join(list(listToRotate))
+	return	''.join(list(listToRotate))
 
 leftrotation("1 2 3 4 5 /n 5")
